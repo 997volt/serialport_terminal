57b5ba27b741ab259c799c730e25bf03feca0ba3
997Volt <piotrhalab@gmail.com>
2020-12-02 20:14:55 +0100

    add dcb structure initialization top com_read

--- lib/port/port.c
+++ lib/port/port.c
@@ -28,7 +28,34 @@ BOOL com_check(int port_number)
 
 int com_read(int port_number)
 {
-    
+    char com_str[12];
+    sprintf(com_str, "\\\\.\\COM%d", port_number);
+
+    HANDLE com_handle = CreateFile( com_str,             
+                        GENERIC_READ | GENERIC_WRITE,
+                        0, NULL, OPEN_EXISTING, 0, NULL);        
+
+    // check if connected succesfully
+    if (com_handle == INVALID_HANDLE_VALUE)
+    {
+        printf("Error opening port COM%d\n", port_number);
+        CloseHandle(com_handle);
+        return 1; 
+    }
+    printf("port opened\n");    
+
+    DCB dcbSerialParams = { 0 };                         // Initializing DCB structure
+    dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
+    dcbSerialParams.BaudRate = CBR_9600;      // Setting BaudRate = 9600
+    dcbSerialParams.ByteSize = 8;             // Setting ByteSize = 8
+    dcbSerialParams.StopBits = ONESTOPBIT;    // Setting StopBits = 1
+    dcbSerialParams.Parity = NOPARITY;        // Setting Parity = None 
+
+    if (GetCommState(com_handle, &dcbSerialParams) == FALSE)
+    {
+        printf("GetCommState() error\n");
+        return 2;
+    }    
 
    
 }
